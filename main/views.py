d35db1b3dd23188375083dd56deb2b74d61c4abc
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-02-15 01:50:40 -0500

    documentation

--- main/views.py
+++ main/views.py
@@ -3,17 +3,21 @@ from .controllers import MenuController
 # Create your views here.
 
 def home(response):
+    """ landing page """
     return MenuController.home(response)
 
 def create(response):
+    """ handle create menu POST """
     return MenuController.create(response)
 
 def view(response, name):
+    """ handle view menu GET """
     return MenuController.view(response, name)
 
 def edit(response, name):
+    """ handle edit menu PUT """
     return MenuController.edit(response, name)
 
 def delete(response, name):
+    """ handle delete menu DELETE """
     return MenuController.delete(response, name)
-
