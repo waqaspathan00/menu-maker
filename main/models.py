57ffbfb898a01a069e3f4455ebc7671d5b0c1d2a
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-02-15 01:06:54 -0500

    serializable Menu objects

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+# Create your models here
 
-# Create your models here.
+class Menu(dict):
+    def __init__(self, name, categories):
+        super().__init__(name=name, categories=categories)
+
+
+class Category(dict):
+    """
+    a category consists of:
+        - its title
+        - the items within it
+    """
+    def __init__(self, title, items):
+        super().__init__(title=title, items=items)
+
+class Item(dict):
+    """
+    individualized menu items that are part of a category
+    """
+    def __init__(self, name, price, description):
+        super().__init__(name=name, price=price, description=description)
