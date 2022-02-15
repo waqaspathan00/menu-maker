539fa636358d1ff8a3ce0156605293c091e8f1a2
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-02-15 01:06:31 -0500

    CRUD routes being handled by MenuController

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,10 @@
-# in main app, urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path arguments:
-    # 1. the specific website address, leaving it blank could imply home page
-    # 2. the function you want to call when this page is reached
-    # 3. a name for the url
-    path("", views.home, name="home"),
+    path("", views.home, name="create"),
+    path("create", views.create, name="create"),
+    path("view/<str:name>", views.view, name="view"),
+    path("edit/<str:name>", views.edit, name="edit"),
+    path("delete/<str:name>", views.delete, name="delete"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,19 @@
-# main.views
-from django.shortcuts import render
+from .controllers import MenuController
 
 # Create your views here.
 
 def home(response):
-    return render(response, "main/index.html", {})
+    return MenuController.home(response)
+
+def create(response):
+    return MenuController.create(response)
+
+def view(response, name):
+    return MenuController.view(response, name)
+
+def edit(response, name):
+    return MenuController.edit(response, name)
+
+def delete(response, name):
+    return MenuController.delete(response, name)
+
