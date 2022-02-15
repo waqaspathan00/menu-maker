37d5f5f4b1480d63f8a113c2469efe7ca921110b
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-02-15 01:05:42 -0500

    created FirestoreDB static instance

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
-
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+class FirestoreDB:
+    cred = credentials.Certificate("serviceAccountKey.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    menu_collection = db.collection("menus")
