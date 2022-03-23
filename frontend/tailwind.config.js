edfccd0de6fbd2aca710194fc72f0087cd4f1a31
cjdomacena <cjdomacena@yahoo.com>
2022-03-23 17:42:09 -0400

    chore: :lipstick: Added custom colors in tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-black': '#1C1C1E',
+        'primary-red': '#FF3B30',
+        'primary-blue':'#007AFF',
+        'primary-gray': '#8E8E93'
+      }
+    },
   },
   plugins: [],
 }
