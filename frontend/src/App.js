1b5a20a0199042d5456f5b2e00f45ffdb4c617ef
cjdomacena <cjdomacena@yahoo.com>
2022-03-23 17:50:18 -0400

    feat: :construction: Started navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
-import {loginWithGoogle} from './firebase/index'
-import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom'
+import { loginWithGoogle } from './firebase/index'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 import MenuPage from './menuPage'
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
+function App()
+{
   return (
     <div className="App">
-        <button onClick={loginWithGoogle}>Login w/ Google</button>
-        <Router>
-          <Routes>
-              {/* <Route exact path='/' component={Home} /> */}
-              <Route path='/view' element={<MenuPage/>} /> 
-          </Routes>
-        </Router>
+      {/* <button onClick={loginWithGoogle}>Login w/ Google</button> */}
+
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* <Route exact path='/' component={Home} /> */}
+          <Route path='/view' element={<MenuPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import Logo from "../Logo/Logo"
+
+const Navbar = () => {
+  return (
+	<nav className="w-full shadow h-[75px]">
+    <Logo />
+  </nav>
+  )
+}
+
+export default Navbar
