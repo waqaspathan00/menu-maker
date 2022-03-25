b768334ed86d51fa748323a86a3937837e4f7132
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-24 23:37:23 -0400

    save the uid from frontend to CustomSession

--- backend/oauth/views.py
+++ backend/oauth/views.py
@@ -1,8 +1,8 @@
 import json
 import jwt
-from django.http import HttpResponse, JsonResponse, request
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from .models import CustomSession
 
 # Create your views here.
 
@@ -10,7 +10,9 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def login(response):
     data = json.loads(response.body)
-    token = jwt.encode(payload=data, key="my_secret_key")
-    response.session['token'] = token
-    print("logging in:", response.session["token"])
+    print("login uid:",data)
+    token = jwt.encode(payload=data, key="my_secret_key", algorithm="HS256")
+    print("login token:",token)
+    new_user = CustomSession(uid=token)  # save user id (login)
+    new_user.save()
     return HttpResponse(status=200)
