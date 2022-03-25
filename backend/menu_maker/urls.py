33cbdf3366fbd618a38ea86a6868a8bfbed85ba8
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-24 23:35:10 -0400

    uncommented /oauth route

--- backend/menu_maker/urls.py
+++ backend/menu_maker/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    # path('oauth/', include('oauth.urls')),
+    path('oauth/', include('oauth.urls')),
 
     # Google Auth:
     # path('', TemplateView.as_view(template_name="index.html")),
