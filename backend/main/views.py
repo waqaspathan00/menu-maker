99f53594aac3a8fe94a7d388703c24bbc684c857
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-25 00:06:07 -0400

    formatting

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,35 +1,39 @@
 from .controllers import MenuController, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from oauth.models import CustomSession
-import json
 import jwt
+
 
 # Create your views here.
 
 def home(response):
     """ landing page """
-    item = CustomSession.objects.order_by('-id')[0].uid
-    print("home token:",item)
-    new = jwt.decode(item, key="my_secret_key", algorithms=["HS256"])
-    print("home uid:",new)
+    jwt_token = CustomSession.objects.order_by('-id')[0].uid  # get jwt token from session
+    print("home token:", jwt_token)
+    jwt_token = jwt.decode(jwt_token, key="my_secret_key", algorithms=["HS256"])  # decode the jwt token
+    print("home uid:", jwt_token)
 
     return HttpResponse(status=200)
     # return JsonResponse(json.dumps(response.body))
     # return MenuController.home(response)
+
 
 @csrf_exempt
 def create(response):
     """ handle create menu POST """
     return MenuController.create(response)
 
+
 def view(response, name):
     """ handle view menu GET """
     return MenuController.view(response, name)
+
 
 @csrf_exempt
 def edit(response, name):
     """ handle edit menu PUT """
     return MenuController.edit(response, name)
+
 
 @csrf_exempt
 def delete(response, name):
