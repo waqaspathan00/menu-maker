a441a333a94b5fc3626971ef7bd85b12fd26efb6
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-24 23:34:28 -0400

    created a custom-made session for uid

--- backend/oauth/models.py
+++ backend/oauth/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomSession(models.Model):
+    """
+    Custom-made session since the one provided by django wasn't working
+    It is as secure as the Django session
+
+    A session is a database with { key: value } pairs
+    This CustomSession can currently only store one { key: value } pair
+
+    # How to work with CustomSession
+
+    from oauth.models import CustomSession
+    session = CustomSession.objects      # get a list of session objects
+    new_user = CustomSession(uid=<UID>)  # save a user to the session (by uid)
+    session.get(id=<ID>).uid                 # get user id
+    session.get(id=<ID>).delete()           # delete user from session (logout)
+    session.all().delete()               # delete all user data in session
+
+    """
+    uid = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.uid
+
+
+# add a row to this table
+# then delete it
+# manually both times
+
+
