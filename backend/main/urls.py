01d21e01b5d02d0d015c8c8d4c6b17ee67493eb8
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-24 23:35:00 -0400

    removed testing route

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="create"),
+    path("", views.home, name="home"),
     path("create", views.create, name="create"),
     path("view/<str:name>", views.view, name="view"),
     path("edit/<str:name>", views.edit, name="edit"),
     path("delete/<str:name>", views.delete, name="delete"),
-    path("login/", views.login, name="login"),
 ]
