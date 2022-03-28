f49da27f22e7a0adc995394b9a04b724469efeac
Sabila Hussain <sabila.hussain127@gmail.com>
2022-03-28 03:29:13 -0400

    updated create function to add menu data under menus collection and menu name under user id

--- backend/main/controllers.py
+++ backend/main/controllers.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse, JsonResponse
 from .apps import FirestoreDB
+from oauth.models import CustomSession
+import jwt
 import json
+
 
 class MenuController:
     """ handle get and post requests concerning food recipes on homepage """
@@ -23,10 +26,30 @@ class MenuController:
             menu_name = data["menu-name"]  # extract menu name
 
             # write menu data to Firestore
-            collection = FirestoreDB.collection(menu_name)
-            collection.document(menu_name).set(data)
+            collection = FirestoreDB.collection("menus")
+            doc = collection.document(menu_name)
 
-            return JsonResponse(data)
+            if doc.get().exists:
+                # menu name taken
+                return HttpResponse(status=409)
+            else:
+                collection.document(menu_name).set(data)
+
+                # add menu name to user collection
+                token = CustomSession.objects.order_by('-id')[0].uid  # get jwt token from session
+                token = jwt.decode(token, key="my_secret_key", algorithms=["HS256"])  # decode the jwt token
+
+                user_menu_doc = FirestoreDB.collection(token).document("menus")
+                menu_names = user_menu_doc.get()
+
+                if menu_names.exists:
+                    menu_names_dict = menu_names.to_dict()
+                    menu_names_dict['menu_names'].append(menu_name)
+                    user_menu_doc.update({'menu_names': menu_names_dict['menu_names']})
+                else:
+                    user_menu_doc.set({'menu_names': [menu_name]})
+
+                return JsonResponse(data)
 
         # on initial page load
         return HttpResponse(status=200)
