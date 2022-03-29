53d8bc4f5a95af4327a3420e5a654f7ac9f16953
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-03-29 19:24:28 -0400

    refactored create logic to be more declarative

--- backend/main/controllers.py
+++ backend/main/controllers.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from .apps import FirestoreDB
-from oauth.models import CustomSession
+# from oauth.models import CustomSession
+from oauth.utilities import get_uid
 import jwt
 import json
 
@@ -22,34 +23,22 @@ class MenuController:
         if request.method == "POST":
             # decode HTTP request using utf-8
             data = json.loads(request.body.decode('utf-8'))
+            menu_name = data["menu-name"]
 
-            menu_name = data["menu-name"]  # extract menu name
-
-            # write menu data to Firestore
-            collection = FirestoreDB.collection("menus")
-            doc = collection.document(menu_name)
-
-            if doc.get().exists:
-                # menu name taken
+            # check if menu name taken
+            menu_doc = FirestoreDB.get_menu(menu_name)
+            if menu_doc:
                 return HttpResponse(status=409)
-            else:
-                collection.document(menu_name).set(data)
 
-                # add menu name to user collection
-                token = CustomSession.objects.order_by('-id')[0].uid  # get jwt token from session
-                token = jwt.decode(token, key="my_secret_key", algorithms=["HS256"])  # decode the jwt token
+            # write menu data "menus" collection
+            FirestoreDB.add_menu(menu_name, data)
 
-                user_menu_doc = FirestoreDB.collection(token).document("menus")
-                menu_names = user_menu_doc.get()
+            # give ownership of the menu_name to the user
+            uid = get_uid()
+            menu_names = FirestoreDB.get_user_menus(uid)
+            FirestoreDB.add_menu_to_user(menu_names, menu_name, uid)
 
-                if menu_names.exists:
-                    menu_names_dict = menu_names.to_dict()
-                    menu_names_dict['menu_names'].append(menu_name)
-                    user_menu_doc.update({'menu_names': menu_names_dict['menu_names']})
-                else:
-                    user_menu_doc.set({'menu_names': [menu_name]})
-
-                return JsonResponse(data)
+            return JsonResponse(data)
 
         # on initial page load
         return HttpResponse(status=200)
