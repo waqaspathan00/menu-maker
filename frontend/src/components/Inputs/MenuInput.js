d527d2dc268180da63af30cad5b42d332d92a00f
cjdomacena <cjdomacena@yahoo.com>
2022-03-31 15:48:09 -0400

    feat: :sparkles: Finished first step of adding a menu

--- frontend/src/components/Inputs/MenuInput.js
+++ frontend/src/components/Inputs/MenuInput.js
@@ -1,20 +1,42 @@
 import { ReactComponent as PizzaIcon } from './../../assets/img/Pizza.svg'
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { adjustStep, storeNewMenu } from '../../redux/menuSlice';
+import { useNavigate } from 'react-router-dom';
 const MenuInput = () =>
 {
+	const [menuName, setMenuName] = useState("")
+	const {data} = useSelector(state => state.newMenu);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	function handleSubmit(e)
+	{
+		e.preventDefault();
+		const temp = {...data};
+		temp['menu-name'] = menuName;
+		dispatch(storeNewMenu(temp));
+		dispatch(adjustStep(2))
+		setMenuName("");
+		navigate("/add-items")
+	}
+
 	return (
 		<div className="mt-4 w-full p-4" data-testid="test-input">
 			<div className="flex items-center space-x-2 font-medium">
+			
 				<PizzaIcon />
 				<h4 className="text-sm">Add Menu</h4>
 			</div>
-			<form className="text-left mt-4">
+			<form className="text-left mt-4" onSubmit={handleSubmit}>
 				<label htmlFor="menu-name">Menu Name *</label>
-				<input type="text" id="menu-name" className="block w-full bg-[#F9F9F9] h-11 border border-[#DADADA] rounded-sm mt-4 pl-4" placeholder="Lunch, dinner, etc." required />
+				<input type="text" id="menu-name" className="block w-full bg-[#F9F9F9] h-11 border border-[#DADADA] rounded-sm mt-4 pl-4" placeholder="Lunch, dinner, etc." value={menuName} required onChange={(e) => setMenuName(e.currentTarget.value)} />
+
+				<div className="flex justify-between items-center mt-8 ">
+					<h4>Cancel</h4>
+					<button className='flex items-center font-bold' type="submit">Next <svg className="w-4 h-4 text-primary-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" /></svg></button>
+				</div>
 			</form>
-			<div className="flex justify-between items-center mt-8 ">
-				<h4>Cancel</h4>
-				<h4 className='flex items-center font-bold'>Next <svg className="w-4 h-4 text-primary-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" /></svg></h4>
-			</div>
 		</div>
 	)
 }
