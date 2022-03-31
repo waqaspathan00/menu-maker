68dfee11131a796d588b039c8e9a3b9b74d38df0
cjdomacena <cjdomacena@yahoo.com>
2022-03-31 15:49:04 -0400

    feat: :construction: Started second step of adding a new menu

---
+++ frontend/src/pages/AddItems.js
@@ -0,0 +1,18 @@
+function AddItems()
+{
+	return (
+		<section className="max-w-[1137px] mx-auto flex items-center 
+		justify-center
+		2xl:space-x-12
+		lg:space-x-12
+		space-x-0
+		flex-wrap
+		mt-0
+		h-[calc(100vh-275px)]
+		">
+			asdaecasd
+		</section>
+	);
+}
+
+export default AddItems;
