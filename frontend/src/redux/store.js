19d4c4e5d50ee1829d7fa19c1e842dd5dd5b9c1f
cjdomacena <cjdomacena@yahoo.com>
2022-03-31 14:49:24 -0400

    feat: :construction: Added redux store
    Store for redux, queries

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import { menuApi } from './menuApi'
+import { getDefaultMiddleware } from "@reduxjs/toolkit";
 export const store = configureStore({
-	reducer: userReducer,
+	reducer: {
+		user: userReducer,
+		[menuApi.reducerPath]: menuApi.reducer
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(menuApi.middleware)
 })
