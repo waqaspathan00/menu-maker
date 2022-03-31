95aa2dfd31ac066a0c2cc1705495c12d308bcdb9
cjdomacena <cjdomacena@yahoo.com>
2022-03-31 14:49:57 -0400

    refactor: :recycle: Removed unused imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,16 @@
 import './App.css';
-import { loginWithGoogle } from './firebase/index'
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import MenuPage from './menuPage'
 import Navbar from './components/Navbar/Navbar';
 import PrimaryLayout from './Layout/PrimaryLayout';
 import AddMenu from './pages/AddMenu';
-
 function App()
 {
   return (
     <div className="App w-screen h-screen">
-      {/* <button onClick={loginWithGoogle}>Login w/ Google</button> */}
-
       <Router>
         <Navbar />
         <Routes>
-          {/* <Route exact path='/' component={Home} /> */}
           <Route path='/view' element={<MenuPage />} />
           <Route path="/dashboard" element={<PrimaryLayout children={<AddMenu />} />} />
         </Routes>
