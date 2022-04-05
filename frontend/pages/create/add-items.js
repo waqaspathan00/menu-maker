6211951452b583b1e03a45fdfaa8589414713587
cjdomacena <cjdomacena@yahoo.com>
2022-04-05 19:50:44 -0400

    refactor: :recycle: Added flag to check if step is equal to this page. If not, go to add menu page

--- frontend/pages/create/add-items.js
+++ frontend/pages/create/add-items.js
@@ -1,14 +1,20 @@
 /* React JS Template using functions */
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext, useEffect } from "react"
 import BreadCrumb from '/components/Breadcrumb/BreadCrumb'
 import { NewMenuContext } from "/lib/context";
+import { useRouter } from 'next/router'
 export default function AddItems()
 {
     const { step, setStep, newMenu } = useContext(NewMenuContext);
-
+    const router = useRouter();
     useEffect(() =>
     {
-        setStep(2);
+        if (step === 1 || step === 3)
+        {
+            setStep(2);
+        } else {
+            router.push("/create/add-menu")
+        }
     }, [])
 
     return (
@@ -17,7 +23,7 @@ export default function AddItems()
                 <BreadCrumb step={step} />
             </div>
             <div>
-                {console.log(newMenu)}
+                {console.log(router.query.step)}
             </div>
         </main>
 
