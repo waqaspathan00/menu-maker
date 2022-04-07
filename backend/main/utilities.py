063a6845133d69f670081a4b38c3af6c8453ab8a
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-04-07 01:49:10 -0400

    if the user is not logged in when this function is called, expect an IndexError when trying to retreive an object from Session

--- backend/main/utilities.py
+++ backend/main/utilities.py
@@ -4,7 +4,11 @@ import jwt
 
 def get_uid():
     """ decode jwt token into uid """
-    token = CustomSession.objects.order_by('-id')[0].uid  # get jwt token from session
-    token = jwt.decode(token, key="my_secret_key", algorithms=["HS256"])  # decode the jwt token
-    uid = token["uid"]  # extract uid from jwt token
-    return uid
+    try:
+        token = CustomSession.objects.order_by('-id')[0].uid  # get jwt token from session
+        token = jwt.decode(token, key="my_secret_key", algorithms=["HS256"])  # decode the jwt token
+        uid = token["uid"]  # extract uid from jwt token
+        return uid
+    except IndexError:
+        return None
+
