1369968cb986380a16a7deeca990c05dfb39510c
cjdomacena <cjdomacena@yahoo.com>
2022-04-07 01:37:14 -0400

    style: :lipstick: Added overflow-hidden to overall component. Remove it if gives you weird layout bug
    Not that important. Remove it if gives you weird layout bug

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,25 +1,28 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import { useUserData } from "../lib/hooks";
 import { NewMenuProvider, UserContext } from "../lib/context";
 import Navbar from "/components/Navbar/Navbar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { async } from '@firebase/util';
+
 function MyApp({ Component, pageProps })
 {
 
     const userData = useUserData();
     const [step, setStep] = useState(1);
-
     return (
-        <UserContext.Provider value={{ userData, step, setStep }}>
-            <title>Menu Mate</title>
-            <Navbar />
-            <NewMenuProvider>
-                <Component {...pageProps} />
-                <ToastContainer position='top-right' />
-            </NewMenuProvider>
-        </UserContext.Provider>
+        <div className='overflow-hidden'>
+            <UserContext.Provider value={{ userData, step, setStep }}>
+                <title>Menu Mate</title>
+                <Navbar />
+                <NewMenuProvider>
+                    <Component {...pageProps} />
+                    <ToastContainer position='top-right' />
+                </NewMenuProvider>
+            </UserContext.Provider>
+        </div>
     )
 }
 
