cf23ef76a4a9ff98f0ac8ba57bbda2050e4e8991
cjdomacena <cjdomacena@yahoo.com>
2022-04-07 01:34:25 -0400

    feat: :sparkles: Added component for displaying single dish

---
+++ frontend/components/ItemList/Dish.js
@@ -0,0 +1,25 @@
+import { BsThreeDotsVertical } from 'react-icons/bs'
+import { MdOutlineDragIndicator } from 'react-icons/md'
+
+function Dish({ props })
+{
+	return (
+		<div className='mt-4 w-auto border rounded shadow-lg p-4'>
+			<div className='flex items-center'>
+				<MdOutlineDragIndicator className='mr-4' />
+				<div className='w-full border-l pl-4'>
+					<div className='w-full flex justify-between items-center'>
+						<h1 className='leading-relaxed font-semibold'>{props['item-name']}</h1>
+						<span className='flex items-center text-xs'>Options<BsThreeDotsVertical className='w-4 h-4' /></span>
+					</div>
+
+					<div className=''>
+						<p className='text-sm leading-relaxed'>{props['item-description']}</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	)
+}
+export default Dish
