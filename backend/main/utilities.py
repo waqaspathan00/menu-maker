e183ccef387b434cd3a434026cfc793bc8abfc94
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-04-07 02:05:45 -0400

    additional attempt to refactor logic in menu controller

--- backend/main/utilities.py
+++ backend/main/utilities.py
@@ -1,5 +1,6 @@
 from .models import CustomSession
 import jwt
+from .apps import FirestoreDB
 
 
 def get_uid():
@@ -11,4 +12,19 @@ def get_uid():
         return uid
     except IndexError:
         return None
+"""
+attempt to refactor logic in edit and delete crud functions
 
+def valid_user_to_menu_relationship(name):
+     # validate ownership between users and menus (when performing requests) 
+    uid = get_uid()
+    user_owned_menus = FirestoreDB.get_user_menus(uid)
+
+    # check if user owns the menu
+    if not user_owned_menus.exists:
+        return HttpResponse(status=401, content="You do not have access to this menu")
+
+    menu_names_list = user_owned_menus.to_dict()['menu_names']
+    if name not in menu_names_list:
+        return HttpResponse(status=401, content="You do not have access to this menu")
+"""
