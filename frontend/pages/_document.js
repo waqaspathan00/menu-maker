6c06c1236f56b4f6ef575173519afc3744dd4c2c
cjdomacena <cjdomacena@yahoo.com>
2022-04-07 01:37:45 -0400

    refactor: :lipstick: Added fonts here instead in global.css

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,17 +1,21 @@
-import {Html, Head, Main, NextScript} from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 import React from "react";
 
-export default function Document() {
+export default function Document()
+{
     return (
         <Html>
             <Head>
-                <meta charSet="utf-8"/>
+                <meta charSet="utf-8" />
                 {/*<meta name="viewport" content="width=device-width, initial-scale=1"/>*/}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,400;0,600;0,800;0,900;1,400&display=swap" rel="stylesheet" />
             </Head>
 
             <body>
-                <Main/>
-                <NextScript/>
+                <Main />
+                <NextScript />
 
             </body>
         </Html>
