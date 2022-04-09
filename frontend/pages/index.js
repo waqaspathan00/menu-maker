82599da0abb0c1f967ea737bc24d21c411a427c4
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-04-09 03:51:09 -0400

    finished adding content and styling to tutorial section

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -29,19 +29,35 @@ const SplashSection = () => {
 
 const TutorialSection = () => {
     return (
-        <div className={"flex flex-col items-center relative bg-blue-gray"}>
-            <h2 className={"text-2xl text-center font-bold my-8"}>Let me show you how in 2 simple steps</h2>
+        <div className={"flex flex-col items-around bg-blue-gray"}>
+            <h2 className={"text-3xl text-center font-bold my-8"}>Let me show you how in 2 simple steps</h2>
             <div className={"flex w-full h-80"}>
                 <div className={"flex flex-col items-center justify-center"}>
                     <h3 className={"text-xl font-bold"}>FIRST</h3>
                     <h4 className={"text-center"}>Enter the items on your menu</h4>
                     <img src={"/assets/img/home/first-step-add.png"} className={"w-28"}/>
                 </div>
-                <div className={"w-3/4"}>
+                <div className={"w-3/4 relative"}>
                     <img src={"/assets/img/home/mobile-make-menu.png"}
-                         className={"w-2/5 styled-img absolute top-28 right-12 -rotate-3"}/>
+                         className={"w-3/4 styled-img absolute right-12 -rotate-3"}/>
                     <img src={"/assets/img/home/mobile-add-item.png"}
-                         className={"w-2/5 styled-img absolute top-40 right-4 rotate-6"}/>
+                         className={"w-3/4 styled-img absolute top-20 right-4 rotate-6"}/>
+                </div>
+            </div>
+
+            <div className={"my-20 flex h-80"}>
+                <div className={"w-1/2"}>
+                    <img src={"/assets/img/home/mobile-view-menu.png"}
+                         className={"styled-img w-2/5 absolute left-2 top-100"}/>
+                </div>
+                <div className={"w-1/2 flex flex-col items-center justify-center pr-8"}>
+                    <img src={"/assets/img/home/second-step-qr.png"} className={"w-16"}/>
+                    <h3 className={"text-xl font-bold my-4"}>SECOND</h3>
+                    <h4 className={"text-center"}>Share your menu by link or QR code</h4>
+                    <div className={"flex items-center my-2"}>
+                        <img src={"/assets/img/home/url.png"} className={"w-5"}/>
+                        <p className={"text-xs text-dark-grey"}>menumate.app/marios</p>
+                    </div>
                 </div>
             </div>
         </div>
