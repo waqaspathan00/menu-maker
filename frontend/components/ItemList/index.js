1377775ffb79e6e9e671a00d82010d2e34978b7a
cjdomacena <cjdomacena@yahoo.com>
2022-04-08 23:04:42 -0400

    feat: :sparkles: Added edit and remove button

--- frontend/components/ItemList/index.js
+++ frontend/components/ItemList/index.js
@@ -1,8 +1,9 @@
 import { useContext, useEffect, useState } from 'react';
-import { AiFillCaretDown } from 'react-icons/ai'
+import { AiFillCaretDown, AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import Dish from './Dish';
 import { ReactSortable } from "react-sortablejs";
 import { NewMenuContext } from '../../lib/context';
+import NewCategory from '../Inputs/NewCategory';
 
 /*
 * props @type {object} - holds single category and its items
@@ -16,6 +17,7 @@ function ItemList({ props, index })
 	const [state, setState] = useState(props.items);
 	const [isEdit, setEdit] = useState(false);
 	const { newMenu, setNewMenu } = useContext(NewMenuContext);
+	const [categoryOpen, setIsCategoryOpen] = useState(false)
 
 	const sortable = (menu) =>
 	{
@@ -33,15 +35,24 @@ function ItemList({ props, index })
 	return (
 
 		<div className="w-auto h-auto p-4 border rounded">
-			<button className='flex items-center justify-between w-full' onClick={() => setIsOpen(prev => !prev)}>
-				<h1 className="font-bold text-lg">{props['category-title']}</h1>
-				<AiFillCaretDown className="w-6" />
-			</button>
+			<div className='flex items-center'>
+				<button className='flex items-center justify-between w-full' onClick={() => setIsOpen(prev => !prev)}>
+					<h1 className="font-bold text-lg">{props['category-title']}</h1>
+					<div className='flex items-center'>
+						<AiFillCaretDown className="w-6" />
+					</div>
+				</button>
+				<div className='space-x-2 flex border-l pl-2 border-l-primary-gray'>
+					<AiFillEdit className='hover:text-primary-gray cursor-pointer' title="Edit Category title" onClick={() => setIsCategoryOpen(true)} />
+					<AiFillDelete className='text-primary-red hover:text-primary-red/50 cursor-pointer' title='Delete'/>
+				</div>
+			</div>
 			<div className='space-y-4'>
 				{isOpen ? <ReactSortable list={state} setList={setState} animation={300} disabled={!isEdit ? false : true} >
 					{state.map((item, index) => <Dish key={index} props={item} category={props['category-title']} isEdit={isEdit} setEdit={setEdit} />)}
 				</ReactSortable> : null}
 			</div>
+			{categoryOpen && <NewCategory type='edit' setIsOpen={setIsCategoryOpen} prevCategory={props['category-title']} />}
 		</div>
 	);
 }
