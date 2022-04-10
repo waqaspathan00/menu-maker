e51e456569d8d781395ef6e25baf4b39854930d0
cjdomacena <cjdomacena@yahoo.com>
2022-04-10 10:03:59 -0400

    refactor: :bug: crossOrigin warning fix

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -9,7 +9,7 @@ export default function Document()
                 <meta charSet="utf-8" />
                 {/*<meta name="viewport" content="width=device-width, initial-scale=1"/>*/}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,400;0,600;0,800;0,900;1,400&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet"/>
             </Head>
