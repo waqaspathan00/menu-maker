5fb94d834b8920487431e740307fa1334252d51d
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-04-09 23:03:53 -0400

    added content and styling for install guide section

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,4 @@
+import React, {Fragment} from "react";
 import {loginWithGoogle, logoutWithGoogle} from "../lib/firebase";
 import Image from "next/Image";
 
@@ -8,14 +9,14 @@ export default function Home({menus}) {
             <SplashSection/>
             <TutorialSection/>
             <WhoIsThisForSection/>
-            {/*<InstallGuide/>*/}
+            <InstallGuide/>
         </div>
     )
 }
 
 const SplashSection = () => {
     return (
-        <div className={"flex flex-col items-center bg-gradient-to-b from-white to-blue-gray"}>
+        <div className={"flex flex-col items-center"}>
             <h1 className={"text-4xl font-rock-salt my-6"}>Menu Mate</h1>
             <h3 className={"text-sm text-center mx-12 my-4"}>QUICKLY AND EASILY CREATE SHAREABLE MENUS</h3>
             <div className={"w-1/2 border-2 border-gray-300"}></div>
@@ -78,7 +79,7 @@ const WhoIsThisForSection = () => {
     }
 
     return (
-        <div className={"flex flex-col"}>
+        <div className={"flex flex-col my-8"}>
             <h2 className={"text-3xl text-center font-bold my-4"}>Who is this for?</h2>
             <h3 className={"text-xl text-center"}>Home-based small business'</h3>
 
@@ -88,6 +89,68 @@ const WhoIsThisForSection = () => {
             <TargetCustomerCard image={"wedding"} title={"Boutiques"}/>
 
 
+        </div>
+    )
+}
+
+const InstallGuide = () => {
+    const InstructionStepCard = ({num, title, image}) => {
+        return (
+            <div className={"flex rounded-xl styled-img overflow-hidden justify-between mx-2 my-4"}>
+                <div className={"bg-lavender font-bold flex items-center justify-center w-1/12"}>
+                    <div className={"bg-white rounded-full text-center w-6"}>
+                        {num}
+                    </div>
+                </div>
+                <div className={"flex flex-col w-5/6 items-center pb-4 pr-4"}>
+                    <h5 className={"text-lg"}>{title}</h5>
+                    <img className={"styled-img w-full"} src={`/assets/img/home/installguide/${image}.jpg`}/>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className={"flex flex-col items-center justify-center bg-blue-gray"}>
+            <h2 className={"text-3xl text-center font-bold my-4"}>Install Menu Mate as an app</h2>
+            <h3 className={"text-xl"}>In just 3 clicks!</h3>
+
+            <InstructionStepCard
+                num={"1"}
+                title={
+                    <Fragment>
+                        <div className={"text-center flex items-center"}>
+                            First, press the<strong>&nbsp;share icon</strong>
+                            <img className={"w-8"} src={"/assets/img/home/installguide/share.png"}/>
+                        </div>
+                    </Fragment>
+                }
+                image={"first-step-share"}
+            />
+
+            <InstructionStepCard
+                num={"2"}
+                title={
+                    <Fragment>
+                        <div className={"text-center"}>
+                            Next, scroll down and press <strong>'Add to Home Screen'</strong>
+                        </div>
+                    </Fragment>
+                }
+                image={"second-step-add-home-screen"}
+            />
+
+            <InstructionStepCard
+                num={"3"}
+                title={
+                    <Fragment>
+                        <div className={"text-center"}>
+                            Lastly, press <strong>'Add'</strong>
+                        </div>
+                    </Fragment>
+                }
+                image={"third-step-press-add"}
+            />
         </div>
     )
 }
