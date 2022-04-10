88b5a32dc589cf2c49de98f41d60eae57fcf0dc5
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-04-10 01:37:21 -0400

    created responsive top-navigation bar that links to info sections

--- frontend/components/Navbar/NavigationBar.js
+++ frontend/components/Navbar/NavigationBar.js
@@ -1,24 +1,34 @@
-import UserNav from "/components/UserNav/UserNav";
-import Logo from "/components/Logo/Logo";
-import { useContext, useEffect } from "react";
-import { loginWithGoogle } from "../../lib/firebase";
-import { UserContext } from "../../lib/context";
-const NavigationBar = () =>
-{
-  const { userData } = useContext(UserContext);
+import React from "react";
+import {Navbar, Nav, Container} from "react-bootstrap";
+import NavbarToggle from "react-bootstrap/NavbarToggle";
 
-  return (
-    <header className="w-screen shadow-sm h-auto">
-      <nav className="flex items-center justify-between container mx-auto h-[75px] p-4">
-        <div>
-          <Logo />
-        </div>
-        <div>
-          {userData.user ? <UserNav user={userData.user} /> : <button onClick={loginWithGoogle}>Login in</button>}
-        </div>
-      </nav>
-    </header>
-  )
+export default function NavigationBar() {
+    return (
+        <>
+            <Navbar collapseOnSelect className={"w-full p-6"} fixed={"top"} expand={"md"}
+                    bg="primary" variant={"dark"}>
+                <Container className={"flex justify-around transition-all"}>
+                    <img className={"w-16 h-16 cursor-pointer"} src="/assets/logo.png" alt="logo"/>
+                    <NavbarToggle aria-controls={"responsive-navbar-nav"}/>
+                    <Navbar.Collapse className={"w-full"} id={"responsive-nav-nav"}>
+                        <Nav className={"w-full justify-around"}>
+                            <Nav.Link className={"px-4 my-2 transition-all hover:border-b-2 hover:border-b-gold"}
+                                      href={"/"}>
+                                How to use
+                            </Nav.Link>
+                            <Nav.Link className={"px-4 my-2 transition-all hover:border-b-2 hover:border-b-gold"}
+                                      href={"/"}>
+                                Who is this for?
+                            </Nav.Link>
+                            <Nav.Link className={"px-4 my-2 transition-all hover:border-b-2 hover:border-b-gold"}
+                                      href={"/"}>
+                                How to install
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
 }
 
-export default NavigationBar
