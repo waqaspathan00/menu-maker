fd52135357b09148140cdb49d2194dd398b864f3
cjdomacena <cjdomacena@yahoo.com>
2022-04-10 20:03:52 -0400

    refactor: :recycle: whitespace changes

--- frontend/pages/create/add-menu.js
+++ frontend/pages/create/add-menu.js
@@ -4,20 +4,22 @@ import Image from 'next/Image'
 import { useContext, useEffect } from 'react'
 import { NewMenuContext } from "/lib/context";
 import BreadCrumb from '/components/Breadcrumb/BreadCrumb'
-
+import { useRouter } from "next/router";
 
 
 const AddMenu = () =>
 {
     const { step, setStep } = useContext(NewMenuContext);
+
+
     useEffect(() =>
     {
         setStep(1);
-
     }, [])
 
     return (
         <main className="mt-4 h-auto grid place-items-center">
+
             <div className="container mx-auto">
                 <BreadCrumb step={step} />
             </div>
