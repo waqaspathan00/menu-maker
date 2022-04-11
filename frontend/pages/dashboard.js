534b8d6a2caa514ea3b04ab464494ee075e83e89
cjdomacena <cjdomacena@yahoo.com>
2022-04-10 20:50:52 -0400

    refactor: :recycle: Added edit and delete in dashboard instead of in components

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -2,19 +2,21 @@ import { BsCalendar2Check } from 'react-icons/bs'
 import { SiDatabricks } from 'react-icons/si'
 import { AiFillPlusCircle } from 'react-icons/ai'
 import { useCallback, useContext, useEffect, useState } from 'react';
-import { MenusContext, UserContext } from '../lib/context';
-import { menuRef, db } from '../lib/firebase'
-import { query, where, getDocs, collection } from 'firebase/firestore'
+import { MenusContext, UserContext, NewMenuContext } from '../lib/context';
+import { menuRef } from '../lib/firebase'
+import { query, where, getDocs } from 'firebase/firestore'
 import { useRouter } from 'next/router';
 import CategoryList from '../components/ItemList/CategoryList';
-import { async } from '@firebase/util';
 import axios from 'axios';
 import Link from 'next/link';
+import { toast } from 'react-toastify';
 function Dashboard()
 {
 	const { userMenus, setUserMenu } = useContext(MenusContext);
+	const { newMenu, setNewMenu } = useContext(NewMenuContext);
 	const { userData } = useContext(UserContext)
 	const router = useRouter();
+	const [loading, setLoading] = useState(false);
 	const getMenuList = useCallback(async (isMounted) =>
 	{
 		if (isMounted) 
@@ -41,7 +43,36 @@ function Dashboard()
 
 
 	}, [setUserMenu])
-
+	function handleEdit(menus)
+	{
+		setNewMenu(menus);
+		router.push({
+			pathname: '/create/add-items',
+			query: { isEdit: true, slug: menus['slug'], step: 2 }
+		})
+	}
+	async function handleDelete(e, menu, index)
+	{
+		e.preventDefault();
+		setLoading(true);
+		try
+		{
+			const req = await fetch(`http://127.0.0.1:8000/api/delete/${ menu['slug'] }`, {
+				method: "DELETE"
+			})
+			let temp = [...userMenus];
+			temp.splice(index, 1);
+			setUserMenu(temp);
+			toast.success("Successfully deleted!");
+		} catch (error)
+		{
+			console.error(error.message || error.description)
+			toast.error(error.message || error.description)
+		} finally
+		{
+			setLoading(false);
+		}
+	}
 	useEffect(() =>
 	{
 		let isMounted = true;
@@ -56,7 +87,7 @@ function Dashboard()
 			}
 		}
 		return () => isMounted = false;
-	}, [userData.user, getMenuList, setUserMenu])
+	}, [userData.user, getMenuList, setUserMenu, loading])
 
 	return (
 		<main className="mt-20">
@@ -102,7 +133,28 @@ function Dashboard()
 							</Link>
 							<div className="w-full h-auto mt-4 space-y-4">
 								{userMenus && userMenus?.length !== 0 ? userMenus.map((menu, index) =>
-									<CategoryList props={menu} index={index} key={index} />
+									<div key={index} className="w-full border p-4 rounded relative">
+										{loading ? <div className="absolute right-4 top-4 text-xs flex items-center">
+											Processing
+											<svg className="animate-spin ml-2 h-5 w-5 text-primary-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+												<circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+												<path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+											</svg>
+										</div> : ""}
+
+										<CategoryList props={menu} index={index} />
+										<div className='flex justify-between mt-4 text-sm'>
+											<div className="space-x-4">
+												<button className="font-semibold" onClick={() => handleEdit(menu)} disabled={loading}>Edit</button>
+												<button className="font-semibold" disabled={loading}>View</button>
+												<button className="font-semibold text-primary-red" disabled={loading} onClick={(e) => handleDelete(e,menu, index)}>Delete</button>
+											</div>
+											<div>
+												<button className="font-semibold text-primary-blue" disabled={loading}>Set active</button>
+											</div>
+										</div>
+									</div>
+
 								) : <h1>No menus available</h1>}
 							</div>
 							<SiDatabricks className='w-6 h-6 absolute -left-12 top-0' />
