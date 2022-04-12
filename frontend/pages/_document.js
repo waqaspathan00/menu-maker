69d1bff95f3b358b1779e798f89f4ee93d885666
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-04-12 02:42:34 -0400

    favicon and manifest links

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,22 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Html, Head, Main, NextScript} from 'next/document'
 import React from "react";
 
-export default function Document()
-{
+export default function Document() {
     return (
         <Html>
             <Head>
-                <meta charSet="utf-8" />
-                {/*<meta name="viewport" content="width=device-width, initial-scale=1"/>*/}
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,400;0,600;0,800;0,900;1,400&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet"/>
+                <meta charSet="utf-8"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-touch-icon.png"/>
+                <link rel="manifest" href="/manifest.json"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,400;0,600;0,800;0,900;1,400&display=swap"
+                    rel="stylesheet"/>
+                <link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap"
+                      rel="stylesheet"/>
             </Head>
 
             <body>
-                <Main />
-                <NextScript />
+                <Main/>
+                <NextScript/>
 
             </body>
         </Html>
