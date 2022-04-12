3b2e9d24ad19f26b48345f1dffb1901eb9d4c4ef
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-04-12 02:42:52 -0400

    show formatted menu name in footer

--- frontend/pages/view/[menuName].js
+++ frontend/pages/view/[menuName].js
@@ -4,25 +4,24 @@ import axios from "axios"
 import {useRouter} from 'next/router'
 import Footer from "../../components/footer";
 import Category from "../../components/category";
-import MenuItem from "../../components/MenuItem";
-
 
 export default function Menu() {
-
-    const [categories, setCategory] = useState([])
+    const [categories, setCategories] = useState([])
     const router = useRouter()
+    const [menuName, setMenuName] = useState("")
 
+    /**
+     * when page loads, load the menu data using menu name in url
+     */
     useEffect(async () => {
-        // GET request using axios inside useEffect React hook
         if (router.isReady) {
-            const menuName = router.query.menuName
+            const menuUrlName = router.query.menuName
 
             try {
-                await axios.get('http://localhost:8000/api/view/' + menuName)
-                    .then((response) => {
-                        const menuData = response.data['menu-data']
-                        setCategory(menuData)
-                    });
+                const response = await axios.get('http://localhost:8000/api/view/' + menuUrlName)
+                const data = response.data
+                setCategories(data['menu-data'])
+                setMenuName(data["menu-name"])
             } catch (error) {
                 console.log(error)
             }
@@ -32,16 +31,16 @@ export default function Menu() {
 
     return (
         <div>
-            <p className="flex justify-center p-4 text-2xl font-logo">Categories</p>
+            <p className="text-center p-4 font-bold text-4xl">Categories</p>
             {categories && categories.map((category, index) => {
                 return (
                     <div className="w-11/12 p-2 mx-auto" key={index}>
-                        <Category>{category}</Category>
-                        <p className="w-full p-2 font-logo text-primary-black">{category['category-title']}</p>
+                        <Category category={category}/>
                     </div>
+
                 )
             })}
-            <Footer className='z-50'>{router.query.menuName}</Footer>
+            <Footer className='z-50'>{menuName}</Footer>
         </div>
     )
 }
