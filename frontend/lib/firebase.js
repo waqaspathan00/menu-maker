4b33f831834435e8b895bfb99e2679616968de08
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-04-11 22:02:37 -0400

    formatting, updated routes

--- frontend/lib/firebase.js
+++ frontend/lib/firebase.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import {initializeApp} from "firebase/app";
 import {getStorage} from 'firebase/storage'
 import {toast} from "react-toastify";
-import { collection, getFirestore } from "firebase/firestore";
+import {collection, getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,15 +22,17 @@ import { collection, getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCkKvXgPbR_jYkjjbG3PDGN6w_jM95Anfg",
-    authDomain: "learnfirebase-544b9.firebaseapp.com",
-    databaseURL: "https://learnfirebase-544b9-default-rtdb.firebaseio.com",
-    projectId: "learnfirebase-544b9",
-    storageBucket: "learnfirebase-544b9.appspot.com",
-    messagingSenderId: "982907877980",
-    appId: "1:982907877980:web:01e53d273d9acfbe24c499",
-    measurementId: "G-GSMF3Y1MKB"
+  apiKey: "AIzaSyCkKvXgPbR_jYkjjbG3PDGN6w_jM95Anfg",
+  authDomain: "learnfirebase-544b9.firebaseapp.com",
+  databaseURL: "https://learnfirebase-544b9-default-rtdb.firebaseio.com",
+  projectId: "learnfirebase-544b9",
+  storageBucket: "learnfirebase-544b9.appspot.com",
+  messagingSenderId: "982907877980",
+  appId: "1:982907877980:web:e31fb3ed1e2ce6e924c499",
+  measurementId: "G-KS0FZEHZ6Y"
 };
+
+
 
 
 // Initialize Firebase
@@ -41,12 +43,12 @@ export const auth = getAuth(app)
 export const db = getFirestore();
 export const storage = getStorage();
 
-export const signInWithGoogle =  () => {
+export const signInWithGoogle = () => {
     signInWithPopup(auth, googleProvider).then(async (result) => {
         const user = result.user;
 
         try {
-            const response = await axios.post('http://localhost:8000/api/login/', {uid: user.uid})
+            const response = await axios.post('http://127.0.0.1:8000/api/login/', {uid: user.uid})
             toast.success("Signed in")
         } catch (error) {
             toast.error("Failed to connect to server")
@@ -56,14 +58,15 @@ export const signInWithGoogle =  () => {
 
 export const signOutWithGoogle = () => {
     signOut(auth).then(async r => {
-        try{
-            const response = await axios.post('http://localhost:8000/api/logout/')
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/logout/')
             toast.success("Successfully signed out")
         } catch (error) {
+            console.log(error)
             toast.error("Failed to connect to server")
         }
     })
 }
 
-export const menuRef = collection(db,'menus');
+export const menuRef = collection(db, 'menus');
 
