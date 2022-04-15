e3ef966aea7eb2d856c0483e32e913d5c0ca250c
ak2454 <ak2454@njit.edu>
2022-04-15 01:59:03 -0400

    implement isNewUser function
    On Sign in check if user is a first time user or returning user, redirect accordingly

--- frontend/lib/firebase.js
+++ frontend/lib/firebase.js
@@ -4,6 +4,8 @@ import {initializeApp} from "firebase/app";
 import {getStorage} from 'firebase/storage'
 import {toast} from "react-toastify";
 import {collection, getFirestore} from "firebase/firestore";
+import  Router  from 'next/router';
+import { useState } from 'react';
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,17 +23,24 @@ import {collection, getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCkKvXgPbR_jYkjjbG3PDGN6w_jM95Anfg",
+//   authDomain: "learnfirebase-544b9.firebaseapp.com",
+//   databaseURL: "https://learnfirebase-544b9-default-rtdb.firebaseio.com",
+//   projectId: "learnfirebase-544b9",
+//   storageBucket: "learnfirebase-544b9.appspot.com",
+//   messagingSenderId: "982907877980",
+//   appId: "1:982907877980:web:e31fb3ed1e2ce6e924c499",
+//   measurementId: "G-KS0FZEHZ6Y"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCkKvXgPbR_jYkjjbG3PDGN6w_jM95Anfg",
-  authDomain: "learnfirebase-544b9.firebaseapp.com",
-  databaseURL: "https://learnfirebase-544b9-default-rtdb.firebaseio.com",
-  projectId: "learnfirebase-544b9",
-  storageBucket: "learnfirebase-544b9.appspot.com",
-  messagingSenderId: "982907877980",
-  appId: "1:982907877980:web:e31fb3ed1e2ce6e924c499",
-  measurementId: "G-KS0FZEHZ6Y"
-};
-
+    apiKey: "AIzaSyDrQZNCYWOAJLfJdfSeOsOIHRktHpuH4Ng",
+    authDomain: "manifest-emblem-318503.firebaseapp.com",
+    projectId: "manifest-emblem-318503",
+    storageBucket: "manifest-emblem-318503.appspot.com",
+    messagingSenderId: "598898664609",
+    appId: "1:598898664609:web:cfd0ebe8c144414efafce0"
+  };
 
 
 
@@ -43,21 +52,30 @@ export const auth = getAuth(app)
 export const db = getFirestore();
 export const storage = getStorage();
 
-export const signInWithGoogle = () => {
-    signInWithPopup(auth, googleProvider).then(async (result) => {
-        const user = result.user;
-        const details = getAdditionalUserInfo(result)
-        // details.isNewUser == True or False
-        // if True then redirect to /register to register a new menu name
-        // then redirect to /create/add-items with new menu name
 
-        try {
-            const response = await axios.post('http://127.0.0.1:8000/api/login/', {uid: user.uid})
-            toast.success("Signed in")
-        } catch (error) {
-            toast.error("Failed to connect to server")
-        }
-    })
+export const signInWithGoogle = () =>  {
+        
+        signInWithPopup(auth, googleProvider).then(async (result) => {
+            const user = result.user;
+            const details = getAdditionalUserInfo(result)
+
+            // console.log(details.isNewUser())
+            // details.isNewUser == True or False
+            // if True then redirect to /register to register a new menu name
+            // then redirect to /create/add-items with new menu name
+    
+            try {
+                const response = await axios.post('http://127.0.0.1:8000/api/login/', {uid: user.uid})
+                toast.success("Signed in")
+
+                if (!details.isNewUser){
+                    Router.push('/register')
+                }
+
+            } catch (error) {
+                toast.error("Failed to connect to server")
+            }
+        })
 }
 
 export const signOutWithGoogle = () => {
